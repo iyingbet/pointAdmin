3d091a311ad1d11ef7cce8389e2c7c03c1a3fcaf
developer <developer@iwin.email>
2020-02-26 20:53:54 +0800

    update

---
+++ precache-manifest.d246d9c5c6a3ff0e2a32fe425dc7b7a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cef54b80b4741884f778fe1dac9ef1c",
+    "url": "https://iypointadmin.chinacloudsites.cn/index.html"
+  },
+  {
+    "revision": "f00d698794b22d175661",
+    "url": "https://iypointadmin.chinacloudsites.cn/static/css/2.6d506cf3.chunk.css"
+  },
+  {
+    "revision": "8f7ca8368e07487e96d6",
+    "url": "https://iypointadmin.chinacloudsites.cn/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f00d698794b22d175661",
+    "url": "https://iypointadmin.chinacloudsites.cn/static/js/2.944d003b.chunk.js"
+  },
+  {
+    "revision": "68aed49bcdcbc067487a3d461801ff7e",
+    "url": "https://iypointadmin.chinacloudsites.cn/static/js/2.944d003b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8f7ca8368e07487e96d6",
+    "url": "https://iypointadmin.chinacloudsites.cn/static/js/main.e127988e.chunk.js"
+  },
+  {
+    "revision": "2a7aa21727a12cb23290",
+    "url": "https://iypointadmin.chinacloudsites.cn/static/js/runtime-main.8541dc7e.js"
+  }
+]);
